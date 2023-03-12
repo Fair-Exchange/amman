bfc21342137a550499e7c936392424b45f7700e9
jeffowy <jjgallow@gmail.com>
2023-03-12 09:16:43 -0600

    ledamint explorer

--- amman-client/src/consts.ts
+++ amman-client/src/consts.ts
@@ -8,4 +8,4 @@ export const LOCALHOST = 'http://127.0.0.1:8899'
  * URL at which the amman explorer is deployed.
  * @category utils
  */
-export const AMMAN_EXPLORER = 'https://amman-explorer.metaplex.com'
+export const AMMAN_EXPLORER = 'https://amman-explorer.ledamint.io'
